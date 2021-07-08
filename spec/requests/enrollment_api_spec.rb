2e3b88be1dd5b6144ca1c18dc8bc5eb54004eb55
Joao Almeida <joaorsalmeida@gmail.com>
2021-07-08 15:33:59 -0300

    Add testes para enrollment API

---
+++ spec/requests/enrollment_api_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Enrollment API' do
+  context 'GET /api/v1/courses/:id/enrollments' do
+    it 'should get all enrollments from course' do
+    end
+
+    it 'should return empty if no enrollments' do
+
+    end
+
+    it 'should return 404 if course does not exists' do
+
+    end
+  end
+end
